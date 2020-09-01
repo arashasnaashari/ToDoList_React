be65158e6e45c9975ec89e69b5eed675c3ac05d4
arash <arash.asnaashari@gmail.com>
2020-09-01 20:57:44 +0430

    Updates

---
+++ precache-manifest.baf9e850eb307d190bc4f6d90e0f71ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c924c4b5b53dcb7fa11892d61c642317",
+    "url": "/ToDoList_React/index.html"
+  },
+  {
+    "revision": "1279125b4ccdb268199f",
+    "url": "/ToDoList_React/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1279125b4ccdb268199f",
+    "url": "/ToDoList_React/static/js/2.8df0d804.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ToDoList_React/static/js/2.8df0d804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "467e573a3a66aebeff69",
+    "url": "/ToDoList_React/static/js/main.b99763b6.chunk.js"
+  },
+  {
+    "revision": "8b254be355b54eeda18d",
+    "url": "/ToDoList_React/static/js/runtime-main.2b6c88c4.js"
+  }
+]);
